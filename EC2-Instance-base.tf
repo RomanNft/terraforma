3cb84312ef6378c762ebff66df6142397f18fb04
RomanNft <118902504+RomanNft@users.noreply.github.com>
2023-07-12 19:20:29 +0300

    Update EC2-Instance-base.tf

--- EC2-Instance-base.tf
+++ EC2-Instance-base.tf
@@ -1,70 +1,70 @@
 // Provider
 provider "aws" {
-  region     = "eu-north-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+    region = "us-east-2"
+    access_key = aws_access_key
+    secret_key = aws_secret_key
+  
 }
 
-// Create ec2 instance
-resource "aws_instance" "EC2-Instance" {
-  availability_zone      = "eu-north-1a"
-  ami                    = "ami-0989fb15ce71ba39e"
-  instance_type          = "t3.micro"
-  key_name               = "sv111"
-  vpc_security_group_ids = [aws_security_group.DefaultTerraformSG.id]
+//  Create ec2 instance
+resource "aws_instance" "Apache" {
+    availability_zone = "us-east-2"
+    ami = "ami-024e6efaf93d85776"
+    instance_type = "t2.micro"
+    key_name = "sv"
+    vpc_security_group_ids = [aws_security_group.DEfatt.id]
 
-  // Create main disk
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = 15
-    volume_type = "standard"
+    //Create main disk
+    ebs_block_device {
+        device_name = "/dev/sda1"
+        volume_size = 10
+        volume_type = "gp2"
+        tags = {
+            Neme = "root-disk"
+        }
+
+            }
+    // User script
+    user_data = file("install.sh")
+
+    // Tags
     tags = {
-      Name = "root-disk"
+        Neme = "EC2-Instance"
     }
-  }
-
-  // User script
-  //user_data = file("files/install.sh")
-
-  // Tags
-  tags = {
-    Name = "EC2-Instance"
-  }
 }
+//Create security group
+resource "aws_security_group" "DEfatt" {
+    name = "DEfatt.id"
+    description = "Allow 22, 80, 443 inboud taffic"
 
-// Create security group
-resource "aws_security_group" "DefaultTerraformSG" {
-  name        = "DefaultTerraformSG"
-  description = "Allow 22, 80, 443 inbound taffic"
+    ingress {
+        description = "Allow HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  ingress {
-    description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+     ingress {
+        description = "Allow HTTPS"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+     ingress {
+        description = "Allow SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+     ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
