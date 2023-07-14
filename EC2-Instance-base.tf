e694c2c05963e4ad76164e2e56da2f5f90edb7ea
RomanNft <118902504+RomanNft@users.noreply.github.com>
2023-07-14 19:19:30 +0300

    Update EC2-Instance-base.tf

--- EC2-Instance-base.tf
+++ EC2-Instance-base.tf
@@ -1,8 +1,8 @@
 // Provider
 provider "aws" {
     region = "us-east-2"
-    access_key = 
-    secret_key = 
+    access_key = AKIAYXVCODWWKPO7X45G
+    secret_key = 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
   
 }
 
