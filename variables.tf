7162d78793c7a2ab8f75403d1991af7b9846013d
RomanNft <118902504+RomanNft@users.noreply.github.com>
2023-07-14 19:58:50 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
   type    = string
-  default = "AKIAYXVCODWWKPO7X45G"
+  default = ""
 }
 
 variable "aws_secret_key" {
   type    = string
-  default = "P2pnuITmXnP0G/XP1t6tcGu4WG6Dx0QK/RowEhon"
+  default = ""
 }
